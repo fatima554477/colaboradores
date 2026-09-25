48a48c8884ff18cbb77e3c3b55453a997778b183
fatima554477 <fatima-arellano@hotmail.com>
2026-09-25 01:04:03 -0600

    Add files via upload

--- includes/class.epcinn.php
+++ includes/class.epcinn.php
@@ -2123,9 +2123,11 @@ FECHA_INGRESO_IMSS, JEFE_DIRECTO_1, JEFE_DIRECTO_2, JEFE_DIRECTO_3,PERMISOS, idR
 		return $row['id'];
 	}
 
-	public function guardar_dircasa1($AUTORIZA_1, $EDIFICIO , $calledir1, $NUMERO_EXTERIOR , $NUMERO_INTERIOR , $NUMERO_INTERIOR_2 , $COLONIA , $ALCALDIA , $C_P , $CIUDAD , $ESTADO , $PAIS , $dircasa11 , $DIRECCION_DE_CASA_1_UBICACION_MAPA){
+        public function guardar_dircasa1($AUTORIZA_1, $EDIFICIO , $calledir1, $NUMERO_EXTERIOR , $NUMERO_INTERIOR , $NUMERO_INTERIOR_2 , $COLONIA , $ALCALDIA , $C_P , $CIUDAD , $ESTADO , $PAIS , $dircasa11 , $DIRECCION_DE_CASA_1_UBICACION_MAPA, $registro_id = 0){
+
 		$conn = $this->db();
-		$existe = $this->revisar_dircasa1();
+		$registro_id = (int)$registro_id;
+
 		$session = isset($_SESSION['id'])?$_SESSION['id']:'';
 		if($session != ''){
 			
@@ -2133,11 +2135,15 @@ FECHA_INGRESO_IMSS, JEFE_DIRECTO_1, JEFE_DIRECTO_2, JEFE_DIRECTO_3,PERMISOS, idR
 		 AUTORIZA_1 = '".$AUTORIZA_1."' ,
 
 
-		NUMERO_INTERIOR_2 = '".$NUMERO_INTERIOR_2."' , COLONIA = '".$COLONIA."' , ALCALDIA = '".$ALCALDIA."' , C_P = '".$C_P."' , CIUDAD = '".$CIUDAD."' , ESTADO = '".$ESTADO."' , PAIS = '".$PAIS."' , dircasa11 = '".$dircasa11."' , DIRECCION_DE_CASA_1_UBICACION_MAPA = '".$DIRECCION_DE_CASA_1_UBICACION_MAPA."' where idRelacion = '".$session."' ; ";
+		NUMERO_INTERIOR_2 = '".$NUMERO_INTERIOR_2."' , COLONIA = '".$COLONIA."' , ALCALDIA = '".$ALCALDIA."' , C_P = '".$C_P."' , CIUDAD = '".$CIUDAD."' , ESTADO = '".$ESTADO."' , PAIS = '".$PAIS."' , dircasa11 = '".$dircasa11."' , DIRECCION_DE_CASA_1_UBICACION_MAPA = '".$DIRECCION_DE_CASA_1_UBICACION_MAPA."' where id = '".$registro_id."' and idRelacion = '".$session."' ; ";
+
 		
 		$var2 = "insert into 01dircasa1 ( EDIFICIO, calledir1, NUMERO_EXTERIOR, NUMERO_INTERIOR, NUMERO_INTERIOR_2, COLONIA, ALCALDIA, C_P, CIUDAD, ESTADO, PAIS, dircasa11, DIRECCION_DE_CASA_1_UBICACION_MAPA, idRelacion,AUTORIZA_1) values ( '".$EDIFICIO."' , '".$calledir1."' , '".$NUMERO_EXTERIOR."' , '".$NUMERO_INTERIOR."' , '".$NUMERO_INTERIOR_2."' , '".$COLONIA."' , '".$ALCALDIA."' , '".$C_P."' , '".$CIUDAD."' , '".$ESTADO."' , '".$PAIS."' , '".$dircasa11."' , '".$DIRECCION_DE_CASA_1_UBICACION_MAPA."' , '".$session."' , '".$AUTORIZA_1."'); ";			
 			
-		if($existe>=1){	
+		if($registro_id > 0){	
+
+
+
 
 		mysqli_query($conn,$var1) or die('P233'.mysqli_error($conn));
 		return "ACTUALIZADO";
@@ -2150,6 +2156,32 @@ FECHA_INGRESO_IMSS, JEFE_DIRECTO_1, JEFE_DIRECTO_2, JEFE_DIRECTO_3,PERMISOS, idR
 		}		
 	}
 
+
+	public function listado_dircasa1(){
+
+		$conn = $this->db();
+
+		$session = isset($_SESSION['id']) ? (int)$_SESSION['id'] : 0;
+
+		return mysqli_query($conn, "select * from 01dircasa1 where idRelacion = '".$session."' order by id desc");
+
+	}
+
+
+
+	public function borrar_dircasa1($registro_id){
+
+		$conn = $this->db();
+
+		$session = isset($_SESSION['id']) ? (int)$_SESSION['id'] : 0;
+
+		$registro_id = (int)$registro_id;
+
+		mysqli_query($conn, "delete from 01dircasa1 where id = '".$registro_id."' and idRelacion = '".$session."'") or die('P245'.mysqli_error($conn));
+
+		return "ELEMENTO BORRADO";
+
+	}
 
 
 /**//**//**//**//*DIRECCION CASA 2*//**//**//**//**/
@@ -2168,20 +2200,20 @@ FECHA_INGRESO_IMSS, JEFE_DIRECTO_1, JEFE_DIRECTO_2, JEFE_DIRECTO_3,PERMISOS, idR
 		$row = mysqli_fetch_array($query, MYSQLI_ASSOC);
 		return $row['id'];
 	}
+        public function guardar_f1cercano(  $FAMILIAR_1_PARENTESCO , $FAMILIAR_1_NOMBRE_1 , $FAMILIAR_1_NOMBRE_2 , $FAMILIAR_1_APELLIDO_MATERNO , $FAMILIAR_1_APELLIDO_PATERNO , $FAMILIAR_1_CELULAR_1 , $FAMILIAR_1_CELULAR_2 , $FAMILIAR_1_TELEFONO_DE_CASA_I , $FAMILIAR_1_CORREO_ELECTRONICO , $FAMILIAR_1_EDIFICIO , $FAMILIAR_1_NUMERO_CALLE , $FAMILIAR_1_NUMERO_EXTERIOR , $FAMILIAR_1_NUMERO_INTERIOR , $FAMILIAR_1_NUMER__INTERIOR_2 , $FAMILIAR_1_COLONIA , $FAMILIAR_1_ALCALDIA , $FAMILIAR_1_C_P , $FAMILIAR_1_CIUDAD , $FAMILIAR_1_ESTADO , $FAMILIAR_1_PAIS , $F1CERCANO1 , $FAMILIAR_1_UBICACION__EN_EL_MAPA, $registro_id = 0){
 
-	public function guardar_dircasa2($AUTORIZA_2, $DIRECCION_CASA_2_EDIFICIO , $calledir2,  $DIRECCION_CASA_2_NUMERO_EXTERIOR , $DIRECCION_CASA_2_INTERIOR , $DIRECCION_CASA_INTERIOR_2 , $DIRECCION_CASA_2_COLONIA , $DIRECCION_CASA_2_ALCALDIA , $DIRECCION_CASA_2_C_P , $DIRECCION_CASA_2_CIUDAD , $DIRECCION_CASA_2_ESTADO , $DIRECCION_CASA_2_PAIS , $dircasa22 , $DIRECCION_DE_CASA_2__UBICACION_EN_EL_MAPA ){
 		$conn = $this->db();
-		$existe = $this->revisar_dircasa2();
+		$registro_id = (int)$registro_id;
 		$session = isset($_SESSION['id'])?$_SESSION['id']:'';
 		if($session != ''){
 			
-		$var1 = "update 01dircasa2 set DIRECCION_CASA_2_EDIFICIO = '".$DIRECCION_CASA_2_EDIFICIO."' , calledir2 = '".$calledir2."' , DIRECCION_CASA_2_NUMERO_EXTERIOR = '".$DIRECCION_CASA_2_NUMERO_EXTERIOR."' , 
-		AUTORIZA_2 = '".$AUTORIZA_2."' , 
-		DIRECCION_CASA_2_INTERIOR = '".$DIRECCION_CASA_2_INTERIOR."' , DIRECCION_CASA_INTERIOR_2 = '".$DIRECCION_CASA_INTERIOR_2."' , DIRECCION_CASA_2_COLONIA = '".$DIRECCION_CASA_2_COLONIA."' , DIRECCION_CASA_2_ALCALDIA = '".$DIRECCION_CASA_2_ALCALDIA."' , DIRECCION_CASA_2_C_P = '".$DIRECCION_CASA_2_C_P."' , DIRECCION_CASA_2_CIUDAD = '".$DIRECCION_CASA_2_CIUDAD."' , DIRECCION_CASA_2_ESTADO = '".$DIRECCION_CASA_2_ESTADO."' , DIRECCION_CASA_2_PAIS = '".$DIRECCION_CASA_2_PAIS."' , dircasa22 = '".$dircasa22."' , DIRECCION_DE_CASA_2__UBICACION_EN_EL_MAPA = '".$DIRECCION_DE_CASA_2__UBICACION_EN_EL_MAPA."' where idRelacion = '".$session."' ; ";
+	$var1 = "update 001familiar1mascercano set FAMILIAR_1_PARENTESCO = '".$FAMILIAR_1_PARENTESCO."' , FAMILIAR_1_NOMBRE_1 = '".$FAMILIAR_1_NOMBRE_1."' , FAMILIAR_1_NOMBRE_2 = '".$FAMILIAR_1_NOMBRE_2."' , FAMILIAR_1_APELLIDO_MATERNO = '".$FAMILIAR_1_APELLIDO_MATERNO."' , FAMILIAR_1_APELLIDO_PATERNO = '".$FAMILIAR_1_APELLIDO_PATERNO."' , FAMILIAR_1_CELULAR_1 = '".$FAMILIAR_1_CELULAR_1."' , FAMILIAR_1_CELULAR_2 = '".$FAMILIAR_1_CELULAR_2."' , FAMILIAR_1_TELEFONO_DE_CASA_I = '".$FAMILIAR_1_TELEFONO_DE_CASA_I."' , FAMILIAR_1_CORREO_ELECTRONICO = '".$FAMILIAR_1_CORREO_ELECTRONICO."' , FAMILIAR_1_EDIFICIO = '".$FAMILIAR_1_EDIFICIO."' , FAMILIAR_1_NUMERO_CALLE = '".$FAMILIAR_1_NUMERO_CALLE."' , FAMILIAR_1_NUMERO_EXTERIOR = '".$FAMILIAR_1_NUMERO_EXTERIOR."' , FAMILIAR_1_NUMERO_INTERIOR = '".$FAMILIAR_1_NUMERO_INTERIOR."' , FAMILIAR_1_NUMER__INTERIOR_2 = '".$FAMILIAR_1_NUMER__INTERIOR_2."' , FAMILIAR_1_COLONIA = '".$FAMILIAR_1_COLONIA."' , FAMILIAR_1_ALCALDIA = '".$FAMILIAR_1_ALCALDIA."' , FAMILIAR_1_C_P = '".$FAMILIAR_1_C_P."' , FAMILIAR_1_CIUDAD = '".$FAMILIAR_1_CIUDAD."' , FAMILIAR_1_ESTADO = '".$FAMILIAR_1_ESTADO."' , FAMILIAR_1_PAIS = '".$FAMILIAR_1_PAIS."' , F1CERCANO1 = '".$F1CERCANO1."' , FAMILIAR_1_UBICACION__EN_EL_MAPA = '".$FAMILIAR_1_UBICACION__EN_EL_MAPA."' where id = '".$registro_id."' and idRelacion = '".$session."' ; ";
+
 		
 		$var2 = "insert into 01dircasa2 ( DIRECCION_CASA_2_EDIFICIO,  calledir2, DIRECCION_CASA_2_NUMERO_EXTERIOR, DIRECCION_CASA_2_INTERIOR, DIRECCION_CASA_INTERIOR_2, DIRECCION_CASA_2_COLONIA, DIRECCION_CASA_2_ALCALDIA, DIRECCION_CASA_2_C_P, DIRECCION_CASA_2_CIUDAD, DIRECCION_CASA_2_ESTADO, DIRECCION_CASA_2_PAIS, dircasa22, DIRECCION_DE_CASA_2__UBICACION_EN_EL_MAPA, idRelacion, AUTORIZA_2) values ( '".$DIRECCION_CASA_2_EDIFICIO."' ,  '".$calledir2."' , '".$DIRECCION_CASA_2_NUMERO_EXTERIOR."' , '".$DIRECCION_CASA_2_INTERIOR."' , '".$DIRECCION_CASA_INTERIOR_2."' , '".$DIRECCION_CASA_2_COLONIA."' , '".$DIRECCION_CASA_2_ALCALDIA."' , '".$DIRECCION_CASA_2_C_P."' , '".$DIRECCION_CASA_2_CIUDAD."' , '".$DIRECCION_CASA_2_ESTADO."' , '".$DIRECCION_CASA_2_PAIS."' , '".$dircasa22."' , '".$DIRECCION_DE_CASA_2__UBICACION_EN_EL_MAPA."' , '".$session."', '".$AUTORIZA_2."' ); ";			
 			
-		if($existe>=1){	
+		if($registro_id > 0){	
+	
 
 		mysqli_query($conn,$var1) or die('P276'.mysqli_error($conn));
 		return "ACTUALIZADO";
@@ -2193,6 +2225,32 @@ FECHA_INGRESO_IMSS, JEFE_DIRECTO_1, JEFE_DIRECTO_2, JEFE_DIRECTO_3,PERMISOS, idR
 		echo "NO HAY UN USUARIO SELECCIONADO";	
 		}		
 	}
+		public function listado_f1cercano(){
+
+		$conn = $this->db();
+
+		$session = isset($_SESSION['id']) ? (int)$_SESSION['id'] : 0;
+
+		return mysqli_query($conn, "select * from 001familiar1mascercano where idRelacion = '".$session."' order by id desc");
+
+	}
+
+
+
+	public function borrar_f1cercano($registro_id){
+
+		$conn = $this->db();
+
+		$session = isset($_SESSION['id']) ? (int)$_SESSION['id'] : 0;
+
+		$registro_id = (int)$registro_id;
+
+		mysqli_query($conn, "delete from 001familiar1mascercano where id = '".$registro_id."' and idRelacion = '".$session."'") or die('P287'.mysqli_error($conn));
+
+		return "ELEMENTO BORRADO";
+
+	}
+
 
 
 /**//**//**//**//*001familiar1mascercano*//**//**//**//**/
